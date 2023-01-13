d175e827bac157de6dc1712e64b6e84624e7c454
flashfish0x <flashfish0x@protonmail.com>
2023-01-13 11:53:45 +0000

    first commit

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='sticky top-0 z-50 grid grid-cols-3 shadow-md py-5'>
+        <div>
+            Seabeam
+        </div>
+        <div>
+        </div>
+        <div>
+            {/* Right */}
+        </div>
+    </header>
+  )
+}
+
+export default Header
