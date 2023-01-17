14da4c2f2d6b4a1013a9371e908a510249208e62
flashfish0x <flashfish0x@protonmail.com>
2023-01-17 16:13:52 +0000

    case sensitive

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
-import VlDeposit from '@/components/vlYcrv/vlDeposit'
+import VlDeposit from '@/components/vlYcrv/VlDeposit'
 
 const inter = Inter({ subsets: ['latin'] })
 
